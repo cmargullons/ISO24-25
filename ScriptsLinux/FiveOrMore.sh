e20bd46808d149ed6d9ca3ee1fad1f02ecd2e0e9
cmargullons <carlosmargullonsanchez@gmail.com>
2025-02-07 12:49:32 +0100

    Update FiveOrMore.sh
    SIN ACABAR

--- ScriptsLinux/FiveOrMore.sh
+++ ScriptsLinux/FiveOrMore.sh
@@ -22,17 +22,36 @@ fi
 if [ ! -d $2 ]; then
 
 	echo "ERROR: El directorio no existe."
+	exit
 
 fi
 
 if [ ! -s $2 ]; then
 
 	echo "ERROR: El directorio esta vacio."
+	exit
 
 fi
 
+echo "Script diseñado por Carlos Margullón Sánchez."
+echo
+
+echo > $1
+
 for a in $2/*.txt; do
 
-	echo "$a"
+	wc=$(wc -l $a | cut -d " " -f 1)
+
+	if [ $wc -ge 5 ]; then
+
+		echo
+		echo "$a"
+		echo
+
+		echo "Insertando nombre de los ficheros .txt de $2 en $1..."
+		echo $a >> $1
+		cat $1
+
+	fi
 
 done
