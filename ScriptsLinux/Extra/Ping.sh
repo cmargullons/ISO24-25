73a76b2b2288125c0ecd86ffcf165a42780be280
cmargullons <carlosmargullonsanchez@gmail.com>
2025-02-11 11:36:56 +0100

    Update Ping.sh
    sin acabar

--- ScriptsLinux/Extra/Ping.sh
+++ ScriptsLinux/Extra/Ping.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
+#Script: Ping.sh
+#Autor: Carlos Margullón Sánchez
+#Fecha: 11-02-2025
 
+clear
+
+if [ $# -ne 1 ]; then
+
+	echo "ERROR: Debes introducir un unico parametro (IP)."
+
+fi
+
+ttl=$(ping $1 -c 1 | cut -d " " -f 13 | cut -d "=" -f 2)
+
+if [ $ttl -eq 128 ];then
+
+	echo "TTL=$ttl: Windows"
+
+fi
+
+if [ $ttl -eq 64 ];then
+
+	echo "TTL=$ttl: Linux"
+
+fi
+
+if [ $ttl -eq 255 ];then
+
+	echo "TTL=$ttl: SunsOS"
+
+fi
